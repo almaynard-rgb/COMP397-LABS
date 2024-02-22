9f2646e700461b4e6f93efd8b4967185ccda5c0e
almaynard-rgb <almaynard2404@gmail.com>
2024-02-21 19:23:39 -0600

    LAB #7
    Completed the in-class instructions/content for the class project during the lecture. - LAB #7

--- COMP397-W24-LABS/Assets/_Scripts/AudioManager.cs
+++ COMP397-W24-LABS/Assets/_Scripts/AudioManager.cs
@@ -4,31 +4,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[System.Serializable]
-public class AudioData
-{
-    public string audioName;
-    public AudioClip audioClip;
-}
-
-
+/// <summary>
+/// This component is scene dependant. It will hold the references for AudioAsset per scene nad
+/// call the the AudioController to play the audio.
+/// </summary>
 public class AudioManager : MonoBehaviour, IObserver
 {
     [SerializeField] private Subject _player;
-    [SerializeField] private List<AudioData> _audios = new List<AudioData>();
-
-    [SerializeField] private AudioSource _sfxSource;
+    [SerializeField] private List<AudioAsset> _audios = new List<AudioAsset>();
+    [SerializeField] private string _sceneMusicName;
 
     void Awake()
     {
-        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Subject>();
+        var playerGO = GameObject.FindGameObjectWithTag("Player");
+        if (playerGO == null) { return; }
+
+        _player = playerGO.GetComponent<Subject>();   
     }
-    void OnEnable() => _player.AddObserver(this);
 
-    void OnDisable() => _player.RemoveObserver(this);
+    void Start()
+    {
+        var musicAsset = _audios.Find(a => a.AudioName == _sceneMusicName);
+        AudioController.Instance.PlayMusic(musicAsset);
+    }
 
 
-    public void OnNotify(PlayerEnums playerEnums)
+    void OnEnable() 
+    {
+        if (_player == null) { return; }
+        _player.AddObserver(this);
+    }
+
+    void OnDisable()
+    {
+        if (_player == null) { return; }
+        _player.RemoveObserver(this);
+    }
+
+
+        public void OnNotify(PlayerEnums playerEnums)
     {
         switch (playerEnums)
         {
@@ -45,8 +59,7 @@ public class AudioManager : MonoBehaviour, IObserver
 
     private void PlayAudioClip(string audioName)
     {
-        var clip = _audios.Find(audio => audio.audioName == audioName).audioClip;
-
-        _sfxSource.PlayOneShot(clip);
+        var audioAsset = _audios.Find(asset => asset.AudioName == audioName);
+        AudioController.Instance.PlaySFX(audioAsset);
     }
 }
--- COMP397-W24-LABS/README.txt
+++ COMP397-W24-LABS/README.txt
@@ -3,3 +3,6 @@
 ## Audio Files
 - Audios provided from dklon at [Jumping Sounds on OpenGameArt.org](https://opengameart.org/content/platformer-jumping-sounds)
 - Audios provided from Fupi at [8bit death whirl on OpenGameArt.org](https://opengameart.org/content/8bit-death-whirl)
+-Audio for music from PatrickLieberkind: https://freesound.org/people/PatrickLieberkind/sounds/396024/
+-Audio for music from joshuaempyre: 
+https://freesound.org/people/joshuaempyre/sounds/251461/
